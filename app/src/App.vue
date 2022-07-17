193e792a16d3fb436ea4aa6b2d1b96e31d55bd8d
pedronogs <phnogs@hotmail.com>
2022-07-17 11:02:30 -0300

    feat: add Quasar to App and start converting css to sass

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,34 +1,26 @@
 <template>
-  <div id="main">
-    <h1>SIMPLE-KANBAN</h1>
-    <router-view />
-  </div>
+	<div id="wrapper">
+		<router-view />
+	</div>
 </template>
 
-<style>
-html, body {
-  width: 100%;
-  height: 100%;
+<style lang="sass">
+	html, body
+		width: 100%
+		height: 100%
+		margin: 0
+		padding: 0
 
-  margin: 0;
-  padding: 0;
-}
+	#app
+		width: 100%
+		height: 100%
 
-#app, #main {
-  width: 100%;
-  height: 100%;
+	#wrapper
+		width: 100%
+		height: 100%
 
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#main {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
+		display: flex
+		flex-direction: column
+		justify-content: center
+		align-items: center
 </style>
--- app/src/main.ts
+++ app/src/main.ts
@@ -3,7 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import { Quasar } from 'quasar';
+import quasarUserOptions from './quasar-user-options';
 
-const app = createApp(App).use(router).use(VueAxios, axios)
+const app = createApp(App).use(Quasar, quasarUserOptions).use(router).use(VueAxios, axios)
 app.provide('axios', app.config.globalProperties.axios) 
 app.mount("#app")
